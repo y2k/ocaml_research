0bf948048aa50c180c5cdaa0a2d1e25e786534d6
y2k <itwisterlx@gmail.com>
2020-05-12 16:04:35 +0300

    add storage example #2

--- src/storage_2.ml
+++ src/storage_2.ml
@@ -1,50 +1,80 @@
-type event = TodoCreated of string | TodoRemoved of string
+type event = TodoCreated of string | TodoRemoved of string | Reseted
 
-type serialized_event = {key: string; data: bytes}
+module Domain = struct
+  let reduce_filter e (_ : int) (filter : string * string list) =
+    let contains s1 s2 =
+      let re = Str.regexp_string s2 in
+      try
+        ignore (Str.search_forward re s1 0) ;
+        true
+      with Not_found -> false
+    in
+    match e with
+    | TodoCreated x ->
+        if contains x (fst filter) then x :: snd filter else snd filter
+    | TodoRemoved x ->
+        snd filter |> List.filter (fun y -> x <> y)
+    | Reseted ->
+        []
 
-let memory_reducer (_ : event) (_ : 'a) : 'a = failwith "???"
+  let reduce_count (e : event) count =
+    match e with
+    | TodoCreated _ ->
+        count + 1
+    | TodoRemoved _ ->
+        count - 1
+    | Reseted ->
+        0
 
-let disk_reducer key e : unit Lwt.t =
-  let _se = {key; data= Marshal.to_bytes e []} in
-  failwith "???"
+  let reduce_last_todos (e : event) last_todos =
+    match e with
+    | TodoCreated x ->
+        let rec take n xs =
+          match n with 0 -> [] | _ -> List.hd xs :: take (n - 1) xs
+        in
+        if List.length last_todos >= 20 then x :: take 20 last_todos
+        else x :: last_todos
+    | TodoRemoved x ->
+        List.filter (fun y -> y <> x) last_todos
+    | Reseted ->
+        []
+end
 
-type db = {count: int; last_todos: string list; filter: string * string list}
+module MainScreen = struct
+  type model = {last_todos: string list; count: int; text: string}
 
-let filter_reducer e db =
-  let contains s1 s2 =
-    let re = Str.regexp_string s2 in
-    try
-      ignore (Str.search_forward re s1 0) ;
-      true
-    with Not_found -> false
-  in
-  match e with
-  | TodoCreated x ->
-      if contains x (fst db.filter) then
-        {db with filter= (fst db.filter, x :: snd db.filter)}
-      else db
-  | TodoRemoved x ->
-      { db with
-        filter= (fst db.filter, snd db.filter |> List.filter (fun y -> x <> y))
-      }
+  type msg = TextChanged of string | CreateTodoPressed
 
-let count_reducer (e : event) db =
-  match e with
-  | TodoCreated _ ->
-      {db with count= db.count + 1}
-  | TodoRemoved _ ->
-      {db with count= db.count - 1}
+  let init : model = {last_todos= []; count= 0; text= ""}
 
-let last_top (e : event) db =
-  match e with
-  | TodoCreated x ->
-      let rec take n xs =
-        match n with 0 -> [] | _ -> List.hd xs :: take (n - 1) xs
-      in
-      if List.length db.last_todos >= 20 then
-        {db with last_todos= x :: take 20 db.last_todos}
-      else {db with last_todos= x :: db.last_todos}
-  | TodoRemoved x ->
-      {db with last_todos= List.filter (fun y -> y <> x) db.last_todos}
+  let sub (e : event) (m : model) : model =
+    { m with
+      last_todos= Domain.reduce_last_todos e m.last_todos
+    ; count= Domain.reduce_count e m.count }
 
-let main_reduce e db = db |> filter_reducer e |> count_reducer e |> last_top e
+  let update (m : model) (msg : msg) =
+    match msg with
+    | TextChanged t ->
+        ({m with text= t}, [])
+    | CreateTodoPressed ->
+        (m, [`SendEvent (TodoCreated m.text)])
+end
+
+module SearchScreen = struct
+  type model =
+    {filter: string; todos: string list; inner_filter: string; page: int}
+
+  type msg = FilterChanged of string | ApplyFilterPressed
+
+  let init = {filter= ""; todos= []; inner_filter= ""; page= 0}
+
+  let sub (e : event) (m : model) =
+    {m with todos= Domain.reduce_filter e m.page (m.inner_filter, m.todos)}
+
+  let update (m : model) (msg : msg) =
+    match msg with
+    | FilterChanged f ->
+        ({m with filter= f}, [])
+    | ApplyFilterPressed ->
+        ({m with inner_filter= m.filter; todos= []; page= 0}, [`ResetEvents])
+end
